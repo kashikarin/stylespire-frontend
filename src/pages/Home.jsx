d9e8cfc18466ce658aadb27abfebbe27b20110cd
kashikarin <karinkashi@gmail.com>
2025-11-02 20:41:23 +0200

    add waves to homepage & animation to modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import { ImageUploader } from "../cmps/ImageUploader"
+import { motion, AnimatePresence } from "framer-motion"
 
 export function Home(){
     const [isModalOpen, setIsModalOpen] = useState(false)
@@ -15,11 +16,34 @@ export function Home(){
             <div className="page-layout home-page-container">
                 <h2 className="home-page-subtitle">See it. Love it. Find it.</h2>
                 <p className="upload-instruction">
-                    Upload an image or paste a link to get instant insights about the style, color palette, and overall aesthetic.
+                    Upload an image to uncover the outfit, colors, and style — and explore similar looks instantly.
                 </p>
                 <button onClick={() => setIsModalOpen(true)}>Follow your inspiration</button> 
             </div>
-            {isModalOpen && <ImageUploader onClose={handleClose}/>}
+            <AnimatePresence>
+                {isModalOpen && 
+                    // <motion.div
+                    //     className="image-uploader-backdrop"
+                    //     initial={{ opacity: 0 }}
+                    //     animate={{ opacity: 1 }}
+                    //     exit={{ opacity: 0 }}
+                    //     transition={{ duration: 0.3 }}
+                    // >
+                        <motion.div
+                            className="upload-section"
+                            initial={{ opacity: 0 }}
+                            animate={{opacity: 1 }}
+                            exit={{  opacity: 0 }}
+                            transition={{ duration: 0.3, ease: "easeOut" }}
+                        >
+                            <ImageUploader onClose={handleClose}/>
+                        {/* </motion.div> */}
+                    </motion.div>}
+            </AnimatePresence>
+            
+            
+                
+                
         </>
         
     )
