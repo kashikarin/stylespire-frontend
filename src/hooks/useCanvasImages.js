5e559447b169d28e159961bbde2bd97fa9d6be48
kashikarin <karinkashi@gmail.com>
2026-01-13 15:28:35 +0200

    fix buildimageurl import

--- src/hooks/useCanvasImages.js
+++ src/hooks/useCanvasImages.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { buildImageUrl } from '../services/board.service'
+import { buildImageUrl } from '../services/board.service.js'
 
 export function useCanvasImages(items, background){
     const [imagesById, setImagesById] = useState({})
